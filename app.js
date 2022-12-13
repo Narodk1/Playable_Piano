36bb32223dd4099428b8fadb36252a2eeee10fa6
yousef <youssefnaro@gmail.com>
2022-12-13 20:32:32 +0100

    Added Event Function And some comments

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-let allKeys=[],
+let allKeys=[];
 const pianoKeys = document.querySelectorAll(".piano-keys .key");
 let audio = new Audio("tunes/a.wav");//by default , audio src is'a'tune
 const playTune = (key) => {
